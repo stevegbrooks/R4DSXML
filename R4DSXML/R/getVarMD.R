aaac81e09da6547ca7e1c653e47a119bc22d13cf
Steven Brooks <steven.brooks@boehringer-ingelheim.com>
2024-11-21 14:17:11 +0800

    revert to previous

--- R4DSXML/R/getVarMD.R
+++ R4DSXML/R/getVarMD.R
@@ -2,11 +2,6 @@ getVarMD <- function(filepath) {
     doc <- xmlTreeParse(filepath, useInternalNodes = T)
     namespaces <- namespaces(doc)
 
-    # Add "def" namespace if not present
-    if (!"def" %in% names(namespaces)) {
-        namespaces["def"] <- "http://www.cdisc.org/ns/def/v2.0"
-    }
-
     # ItemRef
     ItemGroupDef <- getNodeSet(doc, "//ns:ItemGroupDef", namespaces)
     DSName <- getDSName(ItemGroupDef)
@@ -66,71 +61,6 @@ getVarMD <- function(filepath) {
 
     Variable.Metadata <- Variable.Metadata[so, ]
     row.names(Variable.Metadata) <- NULL
-
-    # Add where clause conditions for variables with ValueListDefs
-    valueListDefs <- getNodeSet(doc, "//def:ValueListDef", namespaces)
-    
-    if (length(valueListDefs) > 0) {
-        # Initialize a new column for where conditions
-        Variable.Metadata$Where_Conditions <- NA_character_
-        
-        for (vld in valueListDefs) {
-            # Extract the variable name from the OID (e.g., "VL.LB.LBSPID" -> "LBSPID")
-            var_oid <- xmlGetAttr(vld, "OID")
-            dataset_var <- strsplit(var_oid, "\\.")[[1]]
-            if (length(dataset_var) >= 3) {
-                dataset <- dataset_var[2]
-                var_name <- dataset_var[3]
-                
-                # Get all ItemRefs within this ValueListDef
-                itemRefs <- getNodeSet(vld, ".//ns:ItemRef", namespaces)
-                
-                for (itemRef in itemRefs) {
-                    # Get WhereClauseRef
-                    whereClauseRef <- getNodeSet(itemRef, ".//def:WhereClauseRef", namespaces)
-                    if (length(whereClauseRef) > 0) {
-                        whereClauseOID <- xmlGetAttr(whereClauseRef[[1]], "WhereClauseOID")
-                        
-                        # Get the actual WhereClauseDef
-                        whereClauseDef <- getNodeSet(
-                            doc, 
-                            sprintf("//def:WhereClauseDef[@OID='%s']", whereClauseOID),
-                            namespaces
-                        )
-                        
-                        if (length(whereClauseDef) > 0) {
-                            # Extract RangeCheck information
-                            rangeChecks <- getNodeSet(whereClauseDef[[1]], ".//ns:RangeCheck", namespaces)
-                            conditions <- sapply(rangeChecks, function(rc) {
-                                comparator <- xmlGetAttr(rc, "Comparator")
-                                checkValues <- getNodeSet(rc, ".//ns:CheckValue", namespaces)
-                                values <- sapply(checkValues, xmlValue)
-                                paste(comparator, paste(values, collapse = ", "))
-                            })
-                            
-                            # Find the matching row in Variable.Metadata
-                            row_idx <- which(Variable.Metadata$IGD_Name == dataset & 
-                                           Variable.Metadata$ID_Name == var_name)
-                            
-                            if (length(row_idx) > 0) {
-                                current_condition <- Variable.Metadata$Where_Conditions[row_idx]
-                                new_condition <- paste(conditions, collapse = "; ")
-                                
-                                # Append new condition or create if NA
-                                if (is.na(current_condition)) {
-                                    Variable.Metadata$Where_Conditions[row_idx] <- new_condition
-                                } else {
-                                    Variable.Metadata$Where_Conditions[row_idx] <- paste(
-                                        current_condition, new_condition, sep = "\n"
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     # Add derivation descriptions for derived variables
     if (any(Variable.Metadata$ID_OriginType == "Derived")) {
