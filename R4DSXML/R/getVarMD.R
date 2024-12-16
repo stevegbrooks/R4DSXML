e0be61e622a872957c00a4823ff17797ae2654a8
Steven Brooks <steven.brooks@boehringer-ingelheim.com>
2024-12-16 14:51:37 +0800

    got the where condition cases to populate origintype and origindescription fields for assigned and derived types

--- R4DSXML/R/getVarMD.R
+++ R4DSXML/R/getVarMD.R
@@ -64,29 +64,50 @@ getVarMD <- function(filepath) {
 
     # Add derivation descriptions for derived variables
     if (any(Variable.Metadata$ID_OriginType == "Derived")) {
-        # Get all MethodDef nodes
-        methodDefs <- getNodeSet(doc, "//ns:MethodDef", namespaces)
+        # Cache common lookups at the start
+        methodDefs <- getNodeSet(doc, "//odm:MethodDef|//MethodDef", 
+                               c(odm = "http://www.cdisc.org/ns/odm/v1.3"))
+        if (length(methodDefs) > 0) {
+            method_map <- do.call(rbind, lapply(methodDefs, function(node) {
+                oid <- xmlGetAttr(node, "OID")
+                translated_text <- getNodeSet(node, ".//odm:TranslatedText|.//TranslatedText",
+                                           c(odm = "http://www.cdisc.org/ns/odm/v1.3"))
+                desc <- if (length(translated_text) > 0) {
+                    trimws(xmlValue(translated_text[[1]]))
+                } else {
+                    NA_character_
+                }
+                data.frame(
+                    MethodOID = oid,
+                    Description = desc,
+                    stringsAsFactors = FALSE
+                )
+            }))
+        } else {
+            method_map <- data.frame(
+                MethodOID = character(0),
+                Description = character(0),
+                stringsAsFactors = FALSE
+            )
+        }
 
-        # Create a mapping of OID to Description
-        method_descriptions <- sapply(methodDefs, function(node) {
+        commentDefs <- getNodeSet(doc, "//def:CommentDef", 
+                                c(def = "http://www.cdisc.org/ns/def/v2.0"))
+        comment_map <- do.call(rbind, lapply(commentDefs, function(node) {
             oid <- xmlGetAttr(node, "OID")
-            desc <- xpathSApply(node, ".//ns:TranslatedText", xmlValue, namespaces = namespaces)
-            if (length(desc) > 0) {
-                # Clean up the description text
-                desc <- gsub("[\r\n]", " ", desc[1]) # Replace newlines with spaces
-                desc <- trimws(desc) # Remove leading/trailing whitespace
-                desc <- gsub("\\s+", " ", desc) # Replace multiple spaces with single space
-                return(c(oid, desc))
+            translated_text <- getNodeSet(node, ".//odm:TranslatedText|.//TranslatedText",
+                                       c(odm = "http://www.cdisc.org/ns/odm/v1.3"))
+            desc <- if (length(translated_text) > 0) {
+                trimws(xmlValue(translated_text[[1]]))
+            } else {
+                NA_character_
             }
-            return(c(oid, NA))
-        })
-
-        # Convert to data frame
-        method_map <- data.frame(
-            MethodOID = method_descriptions[1, ],
-            Description = method_descriptions[2, ],
-            stringsAsFactors = FALSE
-        )
+            data.frame(
+                CommentOID = oid,
+                Description = desc,
+                stringsAsFactors = FALSE
+            )
+        }))
 
         # For derived variables, get their descriptions
         derived_vars <- Variable.Metadata$ID_OriginType == "Derived"
@@ -97,7 +118,13 @@ getVarMD <- function(filepath) {
             if (any(dataset_vars)) {
                 constructed_oids <- paste0("MT.", dataset, ".", Variable.Metadata$ID_Name[dataset_vars])
                 matched_descriptions <- method_map$Description[match(constructed_oids, method_map$MethodOID)]
-                Variable.Metadata$ID_OriginDescription[dataset_vars] <- matched_descriptions
+                
+                # Only assign if we found matching descriptions
+                valid_matches <- !is.na(matched_descriptions)
+                if (any(valid_matches)) {
+                    idx <- which(dataset_vars)[valid_matches]
+                    Variable.Metadata$ID_OriginDescription[idx] <- matched_descriptions[valid_matches]
+                }
             }
         }
     }
@@ -159,23 +186,63 @@ getVarMD <- function(filepath) {
             if (length(parent_idx) > 0) {
                 parent_row <- Variable.Metadata[parent_idx[1], ]
                 
-                # Get all ItemRef nodes directly
-                itemRefs <- getNodeSet(vld, ".//def:WhereClauseRef", 
-                                     c(def = "http://www.cdisc.org/ns/def/v2.0"))
-                
-                for (whereClauseRef in itemRefs) {
-                    whereClauseOID <- xmlGetAttr(whereClauseRef, "WhereClauseOID")
-                    condition_idx <- which(where_map[, "oid"] == whereClauseOID)
-                    if (length(condition_idx) > 0) {
-                        new_row <- parent_row
-                        new_row$Where_Condition <- where_map[condition_idx, "condition"]
-                        additional_rows[[length(additional_rows) + 1]] <- new_row
+                itemRefs <- xmlChildren(vld)
+                for (itemRef in itemRefs) {
+                    whereClauseRef <- getNodeSet(itemRef, ".//def:WhereClauseRef", 
+                                               c(def = "http://www.cdisc.org/ns/def/v2.0"))
+                    
+                    if (length(whereClauseRef) > 0) {
+                        whereClauseOID <- xmlGetAttr(whereClauseRef[[1]], "WhereClauseOID")
+                        condition_idx <- which(where_map[, "oid"] == whereClauseOID)
+                        
+                        if (length(condition_idx) > 0) {
+                            new_row <- parent_row
+                            new_row$Where_Condition <- where_map[condition_idx, "condition"]
+                            
+                            itemdef_oid <- xmlGetAttr(itemRef, "ItemOID")
+                            
+                            itemdef_nodes <- getNodeSet(doc, 
+                                sprintf("//odm:ItemDef[@OID='%s']|//ItemDef[@OID='%s']", 
+                                        itemdef_oid, itemdef_oid),
+                                c(odm = "http://www.cdisc.org/ns/odm/v1.3",
+                                  def = "http://www.cdisc.org/ns/def/v2.0"))
+                            
+                            if (length(itemdef_nodes) > 0) {
+                                origin_nodes <- getNodeSet(itemdef_nodes[[1]], 
+                                                         ".//def:Origin",
+                                                         c(def = "http://www.cdisc.org/ns/def/v2.0"))
+                                
+                                if (length(origin_nodes) > 0) {
+                                    origin_type <- xmlGetAttr(origin_nodes[[1]], "Type")
+                                    new_row$ID_OriginType <- origin_type
+                                    
+                                    if (origin_type == "Assigned") {
+                                        comment_oid <- xmlGetAttr(itemdef_nodes[[1]], "def:CommentOID")
+                                        if (!is.null(comment_oid)) {
+                                            desc <- comment_map$Description[comment_map$CommentOID == comment_oid]
+                                            if (length(desc) > 0 && !is.na(desc[1])) {
+                                                new_row$ID_OriginDescription <- desc[1]
+                                            }
+                                        }
+                                    } else if (origin_type == "Derived") {
+                                        method_oid <- xmlGetAttr(itemRef, "MethodOID")
+                                        if (!is.null(method_oid)) {
+                                            desc <- method_map$Description[method_map$MethodOID == method_oid]
+                                            if (length(desc) > 0 && !is.na(desc[1])) {
+                                                new_row$ID_OriginDescription <- desc[1]
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            additional_rows[[length(additional_rows) + 1]] <- new_row
+                        }
                     }
                 }
             }
         }
-    }
-    
+    }    
     # Add Where_Condition column to original Variable.Metadata
     Variable.Metadata$Where_Condition <- NA
     
